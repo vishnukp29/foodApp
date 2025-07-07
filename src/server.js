530fbac68c366a1c55f23260ef6c644f32626cb4
Vishnu KP <98652450+vishnukp29@users.noreply.github.com>
2025-07-07 15:24:25 +0530

    cron.js added

--- src/server.js
+++ src/server.js
@@ -3,9 +3,12 @@ import {ENV} from "./config/env.js";
 import { db } from "./config/db.js";
 import { favoritesTable } from "./db/schema.js";
 import { and, eq } from "drizzle-orm";
+import job from "./config/cron.js";
 
 const app = express();
-const PORT = ENV.PORT || 5001
+const PORT = ENV.PORT || 5001;
+
+if (ENV.NODE_ENV === "production") job.start();
 
 app.use(express.json());
 
